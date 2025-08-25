fd0478b7048fc8623d2be6d9f8d6c49e78352f89
Ronaldo Silveira <contato@expertme.com.br>
2025-08-25 08:52:59 -0300

    alterado URL para utilizar do heroku

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import logo from "../../assets/logoLateral.png";
+import logo from "../../assets/LogoLateral.png";
 import "./navbar.css";
 
 function Navbar(){
